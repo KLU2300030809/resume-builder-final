9c7893f11d4237a427665c92afb8c585ed19e6cc
KLU2300030809 <2300030809@kluniversity.in>
2026-06-22 12:48:19 +0530

    Configure production API URL

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -1,17 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true,
 });
 
-// attach token automatically
 api.interceptors.request.use((config) => {
-  console.log("BASE URL:", config.baseURL);
-  console.log("URL:", config.url);
-  console.log("FINAL:", `${config.baseURL}${config.url}`);
   const token = localStorage.getItem("token");
-  console.log("TOKEN SENT:", token); // DEBUG
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
